ff140434cc91a58f1a0358bd0d35cbf393228ab1
Crispin Joe Kenslin <crispin.biotech2022@tnau.ac.in>
2026-03-11 15:23:14 +0530

    testing credentials

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,7 @@ from . import models
 from .database import engine, SessionLocal
 from .routers import crops, genomics, transcriptomics, metabolomics
 import json
-
+import pandas as pd
 # Drop and recreate all tables to match updated models
 models.Base.metadata.drop_all(bind=engine)
 models.Base.metadata.create_all(bind=engine)
